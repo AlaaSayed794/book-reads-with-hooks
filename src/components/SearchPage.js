539eac683a8c87b007b82ac9be9563a12d4a2bcd
AlaaSayed794 <alaa.elsayed.aly@gmail.com>
2022-10-20 21:56:16 +0200

    implement search

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,8 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Book from './Book'
+import PropTypes from 'prop-types';
+import { useState } from "react";
+import * as BooksAPI from '../BooksAPI'
 
 
-export default function SearchPage() {
+export default function SearchPage({ books, moveBook }) {
+
+    const [showingBooks, setShowingBooks] = useState([])
+
+    const searchBooks = async (query) => {
+        if (query) {
+            try {
+                const apiBooks = await BooksAPI.search(query)
+                console.log(apiBooks)
+                console.log(query)
+                //assign shelves to books already in our shelves
+                let booksWithShelves = apiBooks.map(b => {
+                    if (books.some(item => item.id === b.id)) {
+                        b.shelf = books.find(x => x.id === b.id).shelf
+                    }
+                    return b
+                });
+                setShowingBooks(booksWithShelves && (!books.error) ? booksWithShelves : [])
+            }
+            catch (err) {
+                setShowingBooks([])
+            }
+        }
+        //user cleared search query
+        else {
+            setShowingBooks([])
+        }
+
+    }
     return (
         <div className="search-books">
             <div className="search-books-bar">
@@ -11,12 +43,23 @@ export default function SearchPage() {
                     <input
                         type="text"
                         placeholder="Search by title, author, or ISBN"
+                        onChange={(event) => searchBooks(event.target.value)}
                     />
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    {
+                        showingBooks.map(book => (<Book moveBook={moveBook} key={book.id} book={book} />)
+                        )
+                    }
+                </ol>
             </div>
         </div>
     )
+}
+
+SearchPage.propTypes = {
+    books: PropTypes.array.isRequired,
+    moveBook: PropTypes.func.isRequired
 }
